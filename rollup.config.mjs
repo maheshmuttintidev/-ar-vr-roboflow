1378e932e529a3c1593c8bd62bf9ba22b1eb7011
maheshmuttintidev <maheshmuttintidev@gmail.com>
2024-09-25 19:25:57 +0530

    update rollup.config.mjs

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,28 +1,28 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import babel from '@rollup/plugin-babel';
+import { terser } from 'rollup-plugin-terser';
+import polyfillNode from 'rollup-plugin-polyfill-node';
 import json from '@rollup/plugin-json';
 
 export default {
   input: 'src/index.js',
-  output: {
-    file: 'dist/index.js',
-    format: 'umd', // Change to 'umd' for browser compatibility
-    name: 'arvrroboflowreact', // UMD requires a name for the global variable
-    globals: {
-      react: 'React' // Tell Rollup that React is external (prevent bundling React)
+  output: [
+    {
+      file: 'dist/index.js',
+      format: 'cjs',
     },
-  },
-  external: ['react'], // Don't bundle React, let the app handle it
+    {
+      file: 'dist/index.esm.js',
+      format: 'esm',
+    },
+  ],
   plugins: [
-    resolve({
-      browser: true, // Resolves modules for browser
-    }),
-    commonjs(), // Convert CommonJS modules to ES6
+    resolve(),
+    commonjs(),
     json(), // Handle JSON imports
-    babel({
-      babelHelpers: 'bundled',
-      presets: ['@babel/preset-env', '@babel/preset-react'] // Add React support
-    }),
-  ]
+    polyfillNode(),  // Add this to polyfill Node.js modules for browser
+    babel({ babelHelpers: 'bundled' }),
+    terser(),
+  ],
 };
